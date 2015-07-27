5d2ef18ee16ade94c820aa9d7a7df8a9e2d41c97
kn_F <kn_f@hotmail.com>
2015-07-27 22:48:52 +0200

    Web Interface test

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ function test() {
     //$f3->set('name','world');
     $template=new Template;
     echo $template->render('template.htm');
-    
-    
+
+
     echo "<pre>";
     $m = new Knf\Robot\Machine;
     printf("--- NEW ---\n");
@@ -28,7 +28,7 @@ function test() {
         echo $m->loadCPUInstruction("WRITE 1") ."\n";
         echo $m->loadCPUInstruction("READ 9") ."\n";
         echo $m->loadCPUInstruction("DIV 3") ."\n";
-        
+
         $m->run();
         //var_dump($proc->getStatus());
     }
@@ -37,6 +37,58 @@ function test() {
         var_dump($m);
     }
     echo "</pre>";
+}
+
+function welcome(){
+  Base::instance()->set('content','welcome.htm');
+  $view = new View();
+  echo $view->render('layout.htm');
+}
+
+function testWS(){
+  $view = new View();
+  echo $view->render('testws.htm');
+}
+
+function testAjax() {
+    $return_array = array(); 
+
+    // set values
+    $return_array['title']         = "OK AJAX";
+    $return_array['error']         = "Error 1";
+
+    // send to browser as JSON encoded object
+    echo json_encode($return_array);
+}
+
+function testAjaxGet() {
+
+    echo 'TEST OK';
+}
+
+function testLand(){
+  $view = new View();
+  echo $view->render('testland.htm');
+}
+
+function testLandWS() {
+    $l = new Knf\World\Land;
+    $view = array();
+    $empty_symbol=$l->getEmptyBlock();
+    
+    foreach ($l->getPlot() as $line){
+        $ln = array();
+        foreach ($line as $obj) {
+            if ($obj === NULL) {
+                $ln[] = $empty_symbol;  
+            } else {
+                $ln[] = $obj->getSymbol();
+            }
+        }
+        $view[] = $ln;
+    }
+    
+    echo json_encode($view);
 }
 
 
